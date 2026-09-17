13016baa9a643419ec296ca915e6f0c1f20d6ae4
kanop <luciano.andres@gmail.com>
2026-09-16 22:26:54 -0300

    clase 13

--- foodtruck/urls.py
+++ foodtruck/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.productos, name="productos"),
     path('test/', views.test, name="test"),
-    path('registro/', views.registro, name="registro")
+    path('registro/', views.registro, name="registro"),
+    path('editar/',views.editar, name="editar"), 
+    path('editar/<int:id>',views.editar_producto, name="editar_producto")  
 ]
--- pedidos/views.py
+++ pedidos/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Producto
+from .models import Producto, Categoria
 
 # Create your views here.
 def productos (request):
@@ -12,4 +12,21 @@ def test(request):
     return render (request, "pedidos/prueba.html")
 
 def registro(request):
+    if request.method =="POST":
+        
+        Producto.objects.create(
+            nombre=request.POST["nombre"], 
+            precio=request.POST["precio"], 
+            categoria = Categoria.objects.get(id = request.POST["categoria"])
+        )
+        return redirect ('/')
+
     return render (request,"pedidos/registro.html")
+
+def editar(request):
+    productos = Producto.objects.all()
+    return render (request,"pedidos/editar.html", {"productos":productos})
+
+def editar_producto(request, id):
+    productos = Producto.objects.all()
+    return render (request,"pedidos/editar.html", {"productos":productos})
